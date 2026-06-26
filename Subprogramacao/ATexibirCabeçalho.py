e28d8fe9ef09b9f259eeefa87f7dff550df21f5a
Nicolas Ganzaroli da Silva <007668@libertas.edu.br>
2026-06-25 21:43:36 -0300

    Add files via upload

---
+++ Subprogramacao/ATexibirCabeçalho.py
@@ -0,0 +1,10 @@
+def exibir_cabeçalho(texto):
+    print("*"*len(texto))
+    print(texto)
+    print("*"*len(texto))
+
+exibir_cabeçalho("Menu principal")
+print("1 -Cadastras Cliente")
+print("2 - Buscar no banco de dados")
+print("3 - Sair")
+exibir_cabeçalho("FIM DO SISTEMA")
